333878a0a3b297b2a12ac69ebbcb566ba46965f8
Johnny <slagle@apple.com>
2015-12-14 02:21:27 -0800

    Added Day 4

---
+++ AdventOfCode/Day4.swift
@@ -0,0 +1,22 @@
+//
+//  Day4.swift
+//  AdventOfCode
+//
+//  Created by Johnny Slagle on 12/13/15.
+//  Copyright © 2015 Johnny Slagle. All rights reserved.
+//
+
+import Foundation
+
+class Day4: Day {
+    override func part1() -> Any {
+        return ""
+    }
+    
+//    override func part2() -> Any {
+//    }
+    
+    override func input() -> String? {
+        return "bgvyzdsv"
+    }
+}
