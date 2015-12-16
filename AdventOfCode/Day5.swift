cedc7d9f466fca590e3677c18f749dfe840534f0
Johnny <slagle@apple.com>
2015-12-16 01:24:10 -0700

    Clean up and finished part 2. Also, added RegEx notes so future Johnny knows what I did.

--- AdventOfCode/Day5.swift
+++ AdventOfCode/Day5.swift
@@ -13,38 +13,22 @@ class Day5: Day {
     override func part1() -> Any {
         let words = input()?.componentsSeparatedByCharactersInSet(.newlineCharacterSet()) ?? []
         return words.filter { word -> Bool in
-            
-            let word_range = NSMakeRange(0, word.characters.count)
-            
             // Check naughty words
-//            let naughty_regex = try! NSRegularExpression(pattern: "(ab|cd|pq|xy){1,}", options: NSRegularExpressionOptions())
-//            let naughty = naughty_regex.matchesInString(word, options: .ReportCompletion, range: word_range)
-//            guard naughty.count == 0 else {
-//                return false
-//            }
             
+            // (ab|cd|pq|xy) – matches any one of those double letter combos
             guard word.matches("(ab|cd|pq|xy){1,}") == 0 else {
                 return false
             }
             
             // At least 3 vowels
-//            let vowel_regex = try! NSRegularExpression(pattern: "[aeiou]", options: NSRegularExpressionOptions())
-//            let vowels = vowel_regex.matchesInString(word, options: .ReportCompletion, range: word_range)
-//            guard vowels.count >= 3 else {
-//                return false
-//            }
-            
+            // [aeiou] – matches any of these letters
             guard word.matches("[aeiou]") >= 3 else {
                 return false
             }
             
             // At least a double letter
-//            let double_regex = try! NSRegularExpression(pattern: "([a-z])\\1", options: NSRegularExpressionOptions())
-//            let doubles = double_regex.matchesInString(word, options: .ReportCompletion, range: word_range)
-//            guard doubles.count >= 1 else {
-//                return false
-//            }
-            
+            // ([a-z]) – matches any letter and the ()s make it a group
+            // \\1 – match the result of the first group, again
             guard word.matches("([a-z])\\1") >= 1 else {
                 return false
             }
@@ -56,15 +40,22 @@ class Day5: Day {
     override func part2() -> Any {
         let words = input()?.componentsSeparatedByCharactersInSet(.newlineCharacterSet()) ?? []
         return words.filter { word -> Bool in
-
-            let word_range = NSMakeRange(0, word.characters.count)
             
             // pair of any two letters that appears at least twice in the string without overlapping
+            // Note: RegEx breakdown (for future Johnny)
+            // ([a-z]{2})[a-z]*\\1
+            // ([a-z]{2}) is the first group and it matches any two {2} letters between a-z [a-z]
+            // [a-z]* matches zero or more letters between a-z
+            // \\1 should just be \1 but I have to escape the \. It matches the match of group 1 ([a-z]{2}) again
+            guard word.matches("([a-z]{2})[a-z]*\\1") >= 1 else {
+                return false
+            }
             
             // At least one letter which repeats with exactly one letter between them
-            let letter_hug_regex = try! NSRegularExpression(pattern: "([a-z]{1})[a-z]{1}\\1", options: NSRegularExpressionOptions())
-            let letter_hugs = letter_hug_regex.matchesInString(word, options: .ReportCompletion, range: word_range)
-            guard letter_hugs.count >= 1 else {
+            // ([a-z]{1}) – matches one letter
+            // [a-z]{1} – matches one letter
+            // \\1 – matches the first matched letter, again
+            guard word.matches("([a-z]{1})[a-z]{1}\\1") >= 1 else {
                 return false
             }
             
